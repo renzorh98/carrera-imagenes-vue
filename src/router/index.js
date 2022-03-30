6d4a1d9ced1380bcc325bb143336edc49326e25c
renzo <renzorh98@gmail.com>
2022-03-29 22:00:37 -0500

    Vista CarreraImagenes.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import CarreraImagenes from '../views/CarreraImagenes.vue'
 
 const routes = [
   {
@@ -13,7 +14,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: CarreraImagenes,
   }
 ]
 
